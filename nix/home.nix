139b1c9c69270dc9131fc10756c2f6b78bac5ef7
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2026-03-22 17:41:09 +0530

    add .zshrc in nix

--- nix/home.nix
+++ nix/home.nix
@@ -5,5 +5,7 @@
   home.homeDirectory = "/home/harshmpatil";
   home.stateVersion = "25.11";
   services.hyprpaper.enable = true;
+  programs.zsh.enable = true;
   home.file.".config/hypr".source = ./config/hypr;
+  home.file.".zshrc".source = ../.zshrc;
 }
