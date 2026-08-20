10fbd1a92beb92ce497869ca3a47db1b45d9e8d4
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2026-08-21 01:16:24 +0530

    use github_dark background color and update neovim plugins

--- .config/nvim/plugin/colorscheme.lua
+++ .config/nvim/plugin/colorscheme.lua
@@ -2,5 +2,10 @@ vim.pack.add({
 	'https://github.com/folke/tokyonight.nvim'
 })
 
-require('tokyonight')
-vim.cmd.colorscheme('tokyonight-night')
+require('tokyonight').setup({
+	on_colors = function(colors)
+		colors.bg = '#0D1117';
+	end
+})
+
+vim.cmd.colorscheme('tokyonight')
